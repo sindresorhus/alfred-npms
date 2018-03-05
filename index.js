54ec6634d8707654cc65c3ba00ccac9d928e720d
mwood <matt@cratebind.com>
2018-03-05 09:03:37 -0600

    added ctrl-select to paste package name

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ alfy.fetch('https://api.npms.io/v2/search', {
 					},
 					cmd: {
 						subtitle: `${pkg.version} published at ${dateFormat('yyyy-dd-MM', new Date(pkg.date))} by ${(pkg.author && pkg.author.name) || pkg.publisher.username}`
+					},
+					ctrl: {
+						arg: pkg.name,
+						subtitle: `Paste Package ${pkg.name}`
 					}
 				},
 				quicklookurl: pkg.links.repository && `${pkg.links.repository}#readme`
