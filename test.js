5cf7f80149dfc61c79179c007e94a1c69b139243
mwood <matt@cratebind.com>
2018-03-05 09:13:40 -0600

    fixed tests

--- test.js
+++ test.js
@@ -4,19 +4,21 @@ import alfyTest from 'alfy-test';
 test(async t => {
 	const alfy = alfyTest();
 
-	const result = await alfy('boost-exact:true chalk');  // Ensure chalk is first
+	const result = await alfy('boost-exact:true chalk'); // Ensure chalk is first
 	delete result[0].mods.cmd;
 
 	t.deepEqual(result[0], {
 		title: 'chalk',
-		subtitle: 'Terminal string styling done right. Much color',
+		subtitle: 'Terminal string styling done right',
 		arg: 'https://github.com/chalk/chalk',
-		mods:
-		{
-			alt:
-			{
+		mods: {
+			alt: {
 				arg: 'https://www.npmjs.com/package/chalk',
 				subtitle: 'Open the npm page instead of the GitHub repo'
+			},
+			ctrl: {
+				arg: 'chalk',
+				subtitle: `Paste Package chalk`
 			}
 		},
 		quicklookurl: 'https://github.com/chalk/chalk#readme'
